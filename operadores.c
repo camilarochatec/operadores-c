ca3c714d6de20d29d07590f4f783c1d4193b8379
camilarochatec <camilarocha.tec@gmail.com>
2025-01-03 12:30:34 -0300

    finalização

--- operadores.c
+++ operadores.c
@@ -6,15 +6,21 @@ int main(){
     printf("digite outro número inteiro:\n ");
     scanf("%d", &b);
 
-    printf("a soma de a %d e b %d é = \n", a,b,a+b);
-    printf("a subtração de a %d e b %d é = \n", a,b,a-b);
-    printf("a multiplicação de a %d e b %d é = \n", a,b,a*b);
-    printf("a divisão de a %d e b %d é = \n", a,b,a*b);
-    printf("o resto entre %d e b %d é = \n", a,b,a%b);
-
+    //operadores aritméticos
+    printf("a soma de %d e %d é = %d \n", a,b,a+b);
+    printf("a subtração de %d e %d é = %d \n", a,b,a-b);
+    printf("a multiplicação de %d e %d é = %d \n", a,b,a*b);
+    printf("a divisão de %d e %d é = %d \n", a,b,a/b);
+    printf("o resto entre %d e %d é = %d \n", a,b,a%b);
+    
+    //operadores de comparação
     printf("a é igual b? %d \n", a==b);
+    printf("a é maior que b? %d\n", a>b);
+    printf("a é menor que b? %d\n", a<b);
 
+    //operadores lógicos
+    printf("a e b é maior que zero? %d\n", (a>0) && (b>0));
+    printf("a ou b é menor que 10? %d \n", (a<10) || (b<10));
 
-
-
+    return 0;
 }
